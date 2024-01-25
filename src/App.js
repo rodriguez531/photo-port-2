b6ed7696f92b7a7d129bab42191c85d6072f2d49
rodriguez531 <alexr5408@gmail.com>
2024-01-25 11:06:26 -0500

    nav compont renders appropriate sections when click on

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import ContactForm from './components/Contact';
 import './App.css';
 
 function App() {
+
+  const [ contactSelected, setContactSelected ] = useState(false);
   const [categories] = useState([
     { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
     { name: 'portraits', description: 'Portraits of people in my life' },
@@ -21,12 +23,19 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
         >
       </Nav>
       <main>
-        <ContactForm></ContactForm>
+       {!contactSelected ? (
+        <>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
+        </>
+       ): (
+        <ContactForm></ContactForm>
+       )}
         
       </main>
     </div>
